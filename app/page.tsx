723fc8dcacafc3acecec2991a1a8038aed4f0556
JoshGenereux <josh.genereux13@gmail.com>
2026-03-13 15:50:49 -0400

    working on upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import styles from "./page.module.css";
+
 export default function Home() {
-  return <div className="test">HOME SCREEN</div>;
+  return <div className={styles.homePage}>HOME SCREEN</div>;
 }
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -5,9 +5,12 @@ import Image from "next/image";
 export default function Navbar() {
   return (
     <nav className={styles.navbar}>
-      <Image src="/whoopLogo.png" alt="Whoop Logo" width={100} height={25} />
-      &nbsp;
-      <h1 className={styles.headerName}>Charts</h1>
+      <Link href="/" className={styles.homeLink}>
+        <Image src="/whoopLogo.png" alt="Whoop Logo" width={100} height={25} />
+        &nbsp;
+        <h1 className={styles.headerName}>Charts</h1>
+      </Link>
+
       <div className={styles.linksWrapper}>
         <Link className={styles.link} href="/">
           Home
